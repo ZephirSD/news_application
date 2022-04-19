e1d4b2a96593859a400d2db34130a757d4dc9e7a
ZephirSD <siegfrieddallery@gmail.com>
2022-04-19 18:29:52 +0400

    Création de la barre de recherche

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 void main() {
@@ -6,7 +8,7 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: HexColor("#d3d3d3"),
+        backgroundColor: HexColor("#B7B7B7"),
         // appBar: AppBar(
         //   title: Text('Dédé'),
         //   backgroundColor: Colors.red,
@@ -25,8 +27,51 @@ class News extends StatefulWidget {
 }
 
 class _NewsState extends State<News> {
+  var _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 35),
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  fillColor: HexColor('#d3d3d3'),
+                  filled: true,
+                  hintText: 'Recherche...',
+                  hintStyle: GoogleFonts.montserrat(),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      _controller.clear();
+                    },
+                    icon: Icon(
+                      FontAwesomeIcons.magnifyingGlass,
+                      color: HexColor("#EF7676"),
+                    ),
+                  )),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
